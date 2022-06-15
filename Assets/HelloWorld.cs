39913847f5332d07148365b6ed823f29f1bf5abc
Duyy285 <102913911+Duyy285@users.noreply.github.com>
2022-06-15 21:25:42 +0700

    ghgh

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int xinchao;
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        xinchao = 2;
     }
 }
